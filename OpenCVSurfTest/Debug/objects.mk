e0233e6077c13216a8e2bd2dc6a19ec029b89779
skyfiregao <gaolei420@gmail.com>
2015-07-09 17:49:17 +1200

    deleete projects 2

--- OpenCVSurfTest/Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -lopencv_imgproc -lopencv_gpu -lopencv_core  -lopencv_highgui -lopencv_nonfree -lopencv_features2d
-
