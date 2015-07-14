1d2fedec98140501b678aa577f1c4ab9a9a521ea
skyfiregao <gaolei420@gmail.com>
2015-07-14 13:46:18 +1200

    DS325 arm-linux

---
+++ DS325ArmDriver/README.md
@@ -0,0 +1,3 @@
+DS325 based on Jetson TK1   
+
+Grab aligned depth and color images from DS325 sensor based on it's arm-linux driver
---
+++ DS325ArmDriver/src/DSSDKLiteSample.cpp
@@ -0,0 +1,413 @@
+////////////////////////////////////////////////////////////////////////////////
+// SoftKinetic DepthSense SDK
+//
+// COPYRIGHT AND CONFIDENTIALITY NOTICE - SOFTKINETIC CONFIDENTIAL
+// INFORMATION
+//
+// All rights reserved to SOFTKINETIC SENSORS NV (a
+// company incorporated and existing under the laws of Belgium, with
+// its principal place of business at Boulevard de la Plainelaan 15,
+// 1050 Brussels (Belgium), registered with the Crossroads bank for
+// enterprises under company number 0811 341 454 - "Softkinetic
+// Sensors").
+//
+// The source code of the SoftKinetic DepthSense Camera Drivers is
+// proprietary and confidential information of Softkinetic Sensors NV.
+//
+// For any question about terms and conditions, please contact:
+// info@softkinetic.com Copyright (c) 2002-2015 Softkinetic Sensors NV
+////////////////////////////////////////////////////////////////////////////////
+
+
+#include <stdio.h>
+#include <vector>
+#include <exception>
+
+#include <opencv2/opencv.hpp>
+#include <DepthSense.hxx>
+
+using namespace DepthSense;
+using namespace std;
+using namespace cv;
+
+Context g_context;
+DepthNode g_dnode;
+ColorNode g_cnode;
+AudioNode g_anode;
+
+uint32_t g_aFrames = 0;
+uint32_t g_cFrames = 0;
+uint32_t g_dFrames = 0;
+
+bool g_bDeviceFound = false;
+
+ProjectionHelper* g_pProjHelper = NULL;
+StereoCameraParameters g_scp;
+
+Mat g_videoImage;
+Mat g_depthImage;
+Mat g_MappedColorImgae;
+
+//fps
+time_t start, end;
+char fps[50];
+int counter = 0;
+
+/*----------------------------------------------------------------------------*/
+// New audio sample event handler
+void onNewAudioSample(AudioNode node, AudioNode::NewSampleReceivedData data)
+{
+    printf("A#%u: %d\n",g_aFrames,data.audioData.size());
+    g_aFrames++;
+}
+
+/*----------------------------------------------------------------------------*/
+// New color sample event handler
+void onNewColorSample(ColorNode node, ColorNode::NewSampleReceivedData data)
+{
+	if (counter == 0){
+		    time(&start);
+	}
+
+	int32_t w, h;
+	FrameFormat_toResolution(data.captureConfiguration.frameFormat,&w,&h);
+    //cout<<w<<"  "<<h<<"  color"<<endl;
+
+	for(int i=0; i<h; i++)
+	{
+		Vec3b *bgrData = g_videoImage.ptr<Vec3b>(i);
+		for(int j=0; j<w; j++)
+		{
+		        bgrData[j][0] = data.colorMap[w * i * 3 + j *3];
+				bgrData[j][1] = data.colorMap[w * i * 3 + j *3 + 1];
+				bgrData[j][2] = data.colorMap[w * i * 3 + j *3 + 2];
+		}
+	}
+
+    // fps
+    time(&end);
+    counter++;
+    sprintf(fps, "Fps: %.2f", (double)counter/difftime(end, start));
+    if (counter == (INT_MAX - 1000))
+            counter = 0;
+
+    //putText(g_videoImage, fps , cvPoint(30,30), FONT_HERSHEY_COMPLEX_SMALL, 0.8, cvScalar(0,0,250), 1);
+
+	imshow("color", g_videoImage);
+	waitKey(1);
+
+    //printf("C#%u: %d\n",g_cFrames,data.colorMap.size());
+    g_cFrames++;
+}
+
+cv::Mat getDepthDrawableImage(cv::Mat depth_image)
+{
+
+  cv::Mat drawable;
+  depth_image.convertTo(drawable, CV_8UC1, 255.0/1000);
+  drawable = 255 - drawable;
+
+  return drawable;
+}
+
+/*----------------------------------------------------------------------------*/
+// New depth sample event handler
+void onNewDepthSample(DepthNode node, DepthNode::NewSampleReceivedData data)
+{
+    //printf("Z#%u: %d\n",g_dFrames,data.vertices.size());}
+
+    int32_t w, h;
+    FrameFormat_toResolution(data.captureConfiguration.frameFormat,&w,&h);
+    //cout<<w<<"  "<<h<<"  depth"<<endl;
+
+    int count=0;
+    g_MappedColorImgae.setTo(0);
+    if(data.depthMap!=0 && data.uvMap!=0)
+    {
+    	for(int i=0; i<h; i++)
+    	{
+    		int16_t *depthData = g_depthImage.ptr<int16_t>(i);
+    		Vec3b *colordata   = g_MappedColorImgae.ptr<Vec3b>(i);
+    		for(int j=0; j<w; j++)
+    		{
+    			if(data.uvMap[count].u>=0 && data.uvMap[count].v>=0)
+    			{
+    			    int ycoor = (int)(480*data.uvMap[count].v);
+    			    int xcoor = (int)(640*data.uvMap[count].u);
+
+    			    Vec3b tempColor = g_videoImage.at<Vec3b>(ycoor, xcoor);
+    			    colordata[j][0] = tempColor[0];
+    			    colordata[j][1] = tempColor[1];
+    			    colordata[j][2] = tempColor[2];
+    			}
+
+    			depthData[j] = (int16_t)(data.depthMap[count]);
+
+    			count++;
+    		}
+    	}
+    }
+
+    imshow("depth", getDepthDrawableImage(g_depthImage));
+    imshow("mapped image", g_MappedColorImgae);
+    waitKey(1);
+
+    g_dFrames++;
+
+    // Quit the main loop
+    char key = waitKey(10);
+
+    if (key==27) {
+            printf("Quitting main loop from OpenCV\n");
+            g_context.quit();
+    }
+}
+
+/*----------------------------------------------------------------------------*/
+void configureAudioNode()
+{
+    g_anode.newSampleReceivedEvent().connect(&onNewAudioSample);
+
+    AudioNode::Configuration config = g_anode.getConfiguration();
+    config.sampleRate = 44100;
+
+    try
+    {
+        g_context.requestControl(g_anode,0);
+
+        g_anode.setConfiguration(config);
+
+        g_anode.setInputMixerLevel(0.5f);
+    }
+    catch (ArgumentException& e)
+    {
+        printf("Argument Exception: %s\n",e.what());
+    }
+    catch (UnauthorizedAccessException& e)
+    {
+        printf("Unauthorized Access Exception: %s\n",e.what());
+    }
+    catch (ConfigurationException& e)
+    {
+        printf("Configuration Exception: %s\n",e.what());
+    }
+    catch (StreamingException& e)
+    {
+        printf("Streaming Exception: %s\n",e.what());
+    }
+    catch (TimeoutException&)
+    {
+        printf("TimeoutException\n");
+    }
+}
+
+/*----------------------------------------------------------------------------*/
+void configureDepthNode()
+{
+    g_dnode.newSampleReceivedEvent().connect(&onNewDepthSample);
+
+    DepthNode::Configuration config = g_dnode.getConfiguration();
+    config.frameFormat = FRAME_FORMAT_QVGA;
+    config.framerate = 25;
+    config.mode = DepthNode::CAMERA_MODE_CLOSE_MODE;
+    config.saturation = true;
+
+    g_dnode.setEnableVertices(true);
+    g_dnode.setEnableDepthMap(true);
+    g_dnode.setEnableDepthMapFloatingPoint(true);
+    g_dnode.setEnableVerticesFloatingPoint(true);
+    g_dnode.setEnableUvMap(true);
+
+    try
+    {
+        g_context.requestControl(g_dnode,0);
+
+        g_dnode.setConfiguration(config);
+    }
+    catch (ArgumentException& e)
+    {
+        printf("Argument Exception: %s\n",e.what());
+    }
+    catch (UnauthorizedAccessException& e)
+    {
+        printf("Unauthorized Access Exception: %s\n",e.what());
+    }
+    catch (IOException& e)
+    {
+        printf("IO Exception: %s\n",e.what());
+    }
+    catch (InvalidOperationException& e)
+    {
+        printf("Invalid Operation Exception: %s\n",e.what());
+    }
+    catch (ConfigurationException& e)
+    {
+        printf("Configuration Exception: %s\n",e.what());
+    }
+    catch (StreamingException& e)
+    {
+        printf("Streaming Exception: %s\n",e.what());
+    }
+    catch (TimeoutException&)
+    {
+        printf("TimeoutException\n");
+    }
+
+}
+
+/*----------------------------------------------------------------------------*/
+void configureColorNode()
+{
+    // connect new color sample handler
+    g_cnode.newSampleReceivedEvent().connect(&onNewColorSample);
+
+    ColorNode::Configuration config = g_cnode.getConfiguration();
+    config.frameFormat = FRAME_FORMAT_VGA;
+    config.compression = COMPRESSION_TYPE_MJPEG;
+    config.powerLineFrequency = POWER_LINE_FREQUENCY_50HZ;
+    config.framerate = 25;
+
+    g_cnode.setEnableColorMap(true);
+
+    try
+    {
+        g_context.requestControl(g_cnode,0);
+
+        g_cnode.setConfiguration(config);
+    }
+    catch (ArgumentException& e)
+    {
+        printf("Argument Exception: %s\n",e.what());
+    }
+    catch (UnauthorizedAccessException& e)
+    {
+        printf("Unauthorized Access Exception: %s\n",e.what());
+    }
+    catch (IOException& e)
+    {
+        printf("IO Exception: %s\n",e.what());
+    }
+    catch (InvalidOperationException& e)
+    {
+        printf("Invalid Operation Exception: %s\n",e.what());
+    }
+    catch (ConfigurationException& e)
+    {
+        printf("Configuration Exception: %s\n",e.what());
+    }
+    catch (StreamingException& e)
+    {
+        printf("Streaming Exception: %s\n",e.what());
+    }
+    catch (TimeoutException&)
+    {
+        printf("TimeoutException\n");
+    }
+}
+
+/*----------------------------------------------------------------------------*/
+void configureNode(Node node)
+{
+    if ((node.is<DepthNode>())&&(!g_dnode.isSet()))
+    {
+        g_dnode = node.as<DepthNode>();
+        configureDepthNode();
+        g_context.registerNode(node);
+    }
+
+    if ((node.is<ColorNode>())&&(!g_cnode.isSet()))
+    {
+        g_cnode = node.as<ColorNode>();
+        configureColorNode();
+        g_context.registerNode(node);
+    }
+
+    if ((node.is<AudioNode>())&&(!g_anode.isSet()))
+    {
+        g_anode = node.as<AudioNode>();
+        configureAudioNode();
+        g_context.registerNode(node);
+    }
+}
+
+/*----------------------------------------------------------------------------*/
+void onNodeConnected(Device device, Device::NodeAddedData data)
+{
+    configureNode(data.node);
+}
+
+/*----------------------------------------------------------------------------*/
+void onNodeDisconnected(Device device, Device::NodeRemovedData data)
+{
+    if (data.node.is<AudioNode>() && (data.node.as<AudioNode>() == g_anode))
+        g_anode.unset();
+    if (data.node.is<ColorNode>() && (data.node.as<ColorNode>() == g_cnode))
+        g_cnode.unset();
+    if (data.node.is<DepthNode>() && (data.node.as<DepthNode>() == g_dnode))
+        g_dnode.unset();
+    printf("Node disconnected\n");
+}
+
+/*----------------------------------------------------------------------------*/
+void onDeviceConnected(Context context, Context::DeviceAddedData data)
+{
+    if (!g_bDeviceFound)
+    {
+        data.device.nodeAddedEvent().connect(&onNodeConnected);
+        data.device.nodeRemovedEvent().connect(&onNodeDisconnected);
+        g_bDeviceFound = true;
+    }
+}
+
+/*----------------------------------------------------------------------------*/
+void onDeviceDisconnected(Context context, Context::DeviceRemovedData data)
+{
+    g_bDeviceFound = false;
+    printf("Device disconnected\n");
+}
+
+/*----------------------------------------------------------------------------*/
+int main(int argc, char* argv[])
+{
+	g_videoImage.create(480, 640, CV_8UC3);
+	g_depthImage.create(240, 320, CV_16UC1);
+	g_MappedColorImgae.create(240, 320, CV_8UC3);
+    g_context = Context::createStandalone();
+
+    g_context.deviceAddedEvent().connect(&onDeviceConnected);
+    g_context.deviceRemovedEvent().connect(&onDeviceDisconnected);
+
+    // Get the list of currently connected devices
+    vector<Device> da = g_context.getDevices();
+
+    // We are only interested in the first device
+    if (da.size() >= 1)
+    {
+        g_bDeviceFound = true;
+
+        da[0].nodeAddedEvent().connect(&onNodeConnected);
+        da[0].nodeRemovedEvent().connect(&onNodeDisconnected);
+
+        vector<Node> na = da[0].getNodes();
+
+        printf("Found %u nodes\n",na.size());
+
+        for (int n = 0; n < (int)na.size();n++)
+            configureNode(na[n]);
+    }
+
+    g_context.startNodes();
+
+    g_context.run();
+
+    g_context.stopNodes();
+
+    if (g_cnode.isSet()) g_context.unregisterNode(g_cnode);
+    if (g_dnode.isSet()) g_context.unregisterNode(g_dnode);
+    if (g_anode.isSet()) g_context.unregisterNode(g_anode);
+
+    if (g_pProjHelper)
+        delete g_pProjHelper;
+
+    return 0;
+}
