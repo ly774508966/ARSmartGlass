5fe0b1bafb74ce4ccc7ebb5cca9dc9eb14c486e3
skyfiregao <gaolei420@gmail.com>
2015-07-16 13:26:59 +1200

    OpenCV ORB

---
+++ OpenCVORBBasedDS325/src/SoftkineticCalibration.cpp
@@ -0,0 +1,186 @@
+#include "SoftkineticCalibration.h"
+
+softkineticCalibration::softkineticCalibration()
+{
+	R = Mat::zeros(3, 3, CV_64F);
+	T = Mat::zeros(3, 1, CV_64F);
+	K_depth = Mat::zeros(3, 3, CV_64F);
+	K_rgb   = Mat::zeros(3, 3, CV_64F);
+	Dist_depth = Mat::zeros(5, 1, CV_64F);
+	//Dist_rgb   = Mat::zeros(5, 1, CV_64F);
+
+	//All based on Softkinetic 325 sensor
+	string filename = DS325_MATRIX_FILE;
+	FileStorage fs;
+	fs.open(filename, FileStorage::READ);
+
+	if (!fs.isOpened())
+	{
+		cerr << "Failed to open DS325 matrix file" << filename << endl;
+	}
+
+	fs["R"] >> R;
+	fs["T"] >> T;
+	fs["Intrinsics of Depth camera"] >> K_depth;
+	fs["Intrinsics of Color camera"] >> K_rgb;
+	fs["Distortion of Depth camera"] >> Dist_depth;
+	//fs["Distortion of Color camera"] >> Dist_rgb;
+
+	fs.release();
+
+	invert(K_depth, K_depth_inv);
+	//invert(K_rgb, K_rgb_inv);
+
+	//P_depth.create(3, 1, CV_64F);
+	//P_rgb.create(3, 1, CV_64F);
+
+	M1 = Mat::zeros(3, 3, CV_64F);
+	M2 = Mat::zeros(3, 1, CV_64F);
+	Mat temp(3, 3, CV_64F);
+	//M1 = k_rgb*R*k_depth_inv
+	gemm(K_rgb, R, 1, NULL, NULL, temp, 0);
+	gemm(temp, K_depth_inv, 1, NULL, NULL, M1, 0);
+	//M2 = k_rgb*t
+	gemm(K_rgb, T, 1, NULL, NULL, M2, 0);
+}
+
+softkineticCalibration::~softkineticCalibration()
+{
+}
+
+Mat softkineticCalibration::mapColorToDepth(const Mat& srcDepthImage, const Mat& srcColorImage)
+{
+	Mat dstImage(srcDepthImage.rows, srcDepthImage.cols, CV_8UC3);
+	Mat p_depth(3, 1, CV_64F);         //pixel coordinate in depth image
+	Mat p_rgb(3, 1, CV_64F);           //pixel coordinate in color image
+
+	Mat src;
+	undistort(srcDepthImage, src, K_depth, Dist_depth);
+
+	dstImage.setTo(0);
+
+	for(int row=0; row<src.rows; row++)
+	{
+		ushort* depthSrc     = src.ptr<ushort>(row);
+		Vec3b*  dstImageData = dstImage.ptr<Vec3b>(row);
+		for(int col=0; col<src.cols; col++)
+		{
+
+			double depth = (double)(depthSrc[col]/1000.0);
+			if(depth > 1.0) continue;
+
+			p_depth.at<double>(0,0) = col * depth;
+			p_depth.at<double>(1,0) = row * depth;
+			p_depth.at<double>(2,0) = depth;
+
+			gemm(M1, p_depth, 1, M2, 1, p_rgb, 0);
+
+			ushort row_rgb = cvRound(p_rgb.at<double>(1,0)/p_rgb.at<double>(2,0));
+			ushort col_rgb = cvRound(p_rgb.at<double>(0,0)/p_rgb.at<double>(2,0));
+
+			if(row_rgb>=0 && row_rgb<dstImage.rows && col_rgb>=0 && col_rgb<dstImage.cols)
+			{
+				Vec3b tempColor = srcColorImage.at<Vec3b>(row_rgb, col_rgb);
+				dstImageData[col][0] = tempColor[0];
+				dstImageData[col][1] = tempColor[1];
+				dstImageData[col][2] = tempColor[2];
+			}
+		}
+	}
+
+	return dstImage.clone();
+}
+
+Mat softkineticCalibration::mapDepthToColor(const Mat& srcDepthImage)
+{
+
+	Mat dstImage(srcDepthImage.rows, srcDepthImage.cols, CV_16U);
+	Mat p_depth(3, 1, CV_64F);         //pixel coordinate in depth image
+	Mat p_rgb(3, 1, CV_64F);           //pixel coordinate in color image
+
+	Mat src;
+	undistort(srcDepthImage, src, K_depth, Dist_depth);
+
+	dstImage.setTo(MAX_DISTANCE_MM);
+	for(int row=0; row<src.rows; row++)
+	{
+		ushort* depthSrc = src.ptr<ushort>(row);
+		for(int col=0; col<src.cols; col++)
+		{
+			double depth = (double)(depthSrc[col]/1000.0);
+			if(depth > 1.0) continue;
+
+#ifdef TRANSFORM_BASED_ON_MATRIX
+
+			p_depth.at<double>(0,0) = col * depth;
+			p_depth.at<double>(1,0) = row * depth;
+			p_depth.at<double>(2,0) = depth;
+
+			gemm(M1, p_depth, 1, M2, 1, p_rgb, 0);
+
+			ushort row_rgb = cvRound(p_rgb.at<double>(1,0)/p_rgb.at<double>(2,0));
+			ushort col_rgb = cvRound(p_rgb.at<double>(0,0)/p_rgb.at<double>(2,0));
+			ushort depthDst = cvRound(p_rgb.at<double>(2,0)*1000);
+
+			if(row_rgb>=0 && row_rgb<dstImage.rows && col_rgb>=0 && col_rgb<dstImage.cols)
+			{
+				dstImage.at<ushort>(row_rgb, col_rgb) = depthDst;
+			}
+
+#else
+			P_depth.at<double>(0,0) = (col - K_depth.at<double>(0, 2)) * (double)(depthSrc[col]/1000.0) / K_depth.at<double>(0, 0);
+			P_depth.at<double>(1,0) = (row - K_depth.at<double>(1, 2)) * (double)(depthSrc[col]/1000.0) / K_depth.at<double>(1, 1);
+			P_depth.at<double>(2,0) = (double)(depthSrc[col]/1000.0);
+
+			temp.at<double>(0,0)  = R.at<double>(0,0)*P_depth.at<double>(0,0) + R.at<double>(0,1)*P_depth.at<double>(1,0) + R.at<double>(0,2)*P_depth.at<double>(2,0);
+			temp.at<double>(1,0)  = R.at<double>(1,0)*P_depth.at<double>(0,0) + R.at<double>(1,1)*P_depth.at<double>(1,0) + R.at<double>(1,2)*P_depth.at<double>(2,0);
+			temp.at<double>(2,0)  = R.at<double>(2,0)*P_depth.at<double>(0,0) + R.at<double>(2,1)*P_depth.at<double>(1,0) + R.at<double>(2,2)*P_depth.at<double>(2,0);
+			P_rgb.at<double>(0,0) = temp.at<double>(0,0) + T.at<double>(0,0);
+			P_rgb.at<double>(1,0) = temp.at<double>(1,0) + T.at<double>(1,0);
+			P_rgb.at<double>(2,0) = temp.at<double>(2,0) + T.at<double>(2,0);
+
+			p_rgb.at<double>(0,0) = (P_rgb.at<double>(0,0) * K_rgb.at<double>(0, 0) / P_rgb.at<double>(2,0)) + K_rgb.at<double>(0, 2);
+			p_rgb.at<double>(1,0) = (P_rgb.at<double>(1,0) * K_rgb.at<double>(1, 1) / P_rgb.at<double>(2,0)) + K_rgb.at<double>(1, 2);
+
+			ushort row_rgb = cvRound(p_rgb.at<double>(1,0));
+			ushort col_rgb = cvRound(p_rgb.at<double>(0,0));
+			if(row_rgb>=0 && row_rgb<dstImage.rows && col_rgb>=0 && col_rgb<dstImage.cols)
+			{
+				dstImage.at<ushort>(row_rgb, col_rgb) = cvRound(P_rgb.at<double>(2,0)*1000);
+			}
+#endif
+		}
+	}
+
+	return dstImage.clone();
+}
+
+Mat softkineticCalibration::smoothMappedDepthImage(Mat& srcMappedDepthImage)
+{
+	for(int row=0; row<srcMappedDepthImage.rows; row++)
+	{
+		ushort* depthSrc = srcMappedDepthImage.ptr<ushort>(row);
+		for(int col=0; col<srcMappedDepthImage.cols; col++)
+		{
+			if(depthSrc[col] == MAX_DISTANCE_MM && row>0 && row<srcMappedDepthImage.rows-1 && col>0 && col<srcMappedDepthImage.cols-1)
+			{
+				int neighbor_with_depth_num = 0;
+				int distanc_sum = 0;
+
+				if(srcMappedDepthImage.at<ushort>(row-1, col-1) != MAX_DISTANCE_MM) {neighbor_with_depth_num++; distanc_sum += srcMappedDepthImage.at<ushort>(row-1, col-1);}
+				if(srcMappedDepthImage.at<ushort>(row-1, col  ) != MAX_DISTANCE_MM) {neighbor_with_depth_num++; distanc_sum += srcMappedDepthImage.at<ushort>(row-1, col  );}
+				if(srcMappedDepthImage.at<ushort>(row-1, col+1) != MAX_DISTANCE_MM) {neighbor_with_depth_num++; distanc_sum += srcMappedDepthImage.at<ushort>(row-1, col+1);}
+				if(srcMappedDepthImage.at<ushort>(row  , col-1) != MAX_DISTANCE_MM) {neighbor_with_depth_num++; distanc_sum += srcMappedDepthImage.at<ushort>(row  , col-1);}
+				if(srcMappedDepthImage.at<ushort>(row  , col+1) != MAX_DISTANCE_MM) {neighbor_with_depth_num++; distanc_sum += srcMappedDepthImage.at<ushort>(row  , col+1);}
+				if(srcMappedDepthImage.at<ushort>(row+1, col-1) != MAX_DISTANCE_MM) {neighbor_with_depth_num++; distanc_sum += srcMappedDepthImage.at<ushort>(row+1, col-1);}
+				if(srcMappedDepthImage.at<ushort>(row+1, col  ) != MAX_DISTANCE_MM) {neighbor_with_depth_num++; distanc_sum += srcMappedDepthImage.at<ushort>(row+1, col  );}
+				if(srcMappedDepthImage.at<ushort>(row+1, col+1) != MAX_DISTANCE_MM) {neighbor_with_depth_num++; distanc_sum += srcMappedDepthImage.at<ushort>(row+1, col+1);}
+
+				if(neighbor_with_depth_num >= 5)
+					depthSrc[col] = distanc_sum / neighbor_with_depth_num;
+			}
+		}
+	}
+
+	return srcMappedDepthImage;
+}
---
+++ OpenCVORBBasedDS325/src/main.cpp
@@ -0,0 +1,193 @@
+#include <iostream>
+#include <vector>
+#include <openni/OpenNI.h>
+#include <opencv2/core/core.hpp>
+#include <opencv2/features2d/features2d.hpp>
+#include <opencv2/nonfree/features2d.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/nonfree/nonfree.hpp>
+#include <opencv2/nonfree/gpu.hpp>
+#include <opencv2/gpu/gpu.hpp>
+
+#include "SoftkineticCalibration.h"
+
+using namespace std;
+
+cv::Mat getColorImage(openni::VideoFrameRef& color_frame)
+{
+  if(!color_frame.isValid())
+  {
+    return cv::Mat();
+  }
+  openni::VideoMode video_mode = color_frame.getVideoMode();
+  cv::Mat color_img = cv::Mat(video_mode.getResolutionY(),
+                              video_mode.getResolutionX(),
+                              CV_8UC3, (char*)color_frame.getData());
+  cv::Mat ret_img;
+  cv::cvtColor(color_img, ret_img, CV_RGB2BGR);
+  return ret_img;
+}
+
+
+// CV_16U
+cv::Mat getDepthImage(openni::VideoFrameRef& depth_frame)
+{
+  if(!depth_frame.isValid())
+  {
+    return cv::Mat();
+  }
+
+  openni::VideoMode video_mode = depth_frame.getVideoMode();
+  cv::Mat depth_img = cv::Mat(video_mode.getResolutionY(),
+                              video_mode.getResolutionX(),
+                              CV_16U, (char*)depth_frame.getData());
+
+  return depth_img.clone();
+}
+
+
+cv::Mat getDepthDrawableImage(cv::Mat depth_image)
+{
+
+  cv::Mat drawable;
+  depth_image.convertTo(drawable, CV_8UC1, 255.0/1000);
+  drawable = 255 - drawable;
+
+  return drawable;
+}
+
+int main()
+{
+  softkineticCalibration DS325Cali;
+
+  openni::Device device;
+  openni::VideoStream color_stream, depth_stream;
+  const char* deviceURI = openni::ANY_DEVICE;
+
+  openni::Status rc = openni::STATUS_OK;
+
+  rc = openni::OpenNI::initialize();
+
+  //cout << "After initialization:" << openni::OpenNI::getExtendedError();
+
+  rc = device.open(deviceURI);
+  if (rc != openni::STATUS_OK)
+  {
+    cout << "SimpleViewer: Device open failed:" << openni::OpenNI::getExtendedError();
+    openni::OpenNI::shutdown();
+    return 1;
+  }
+
+  rc = depth_stream.create(device, openni::SENSOR_DEPTH);
+  if (rc == openni::STATUS_OK)
+  {
+    rc = depth_stream.start();
+    if (rc != openni::STATUS_OK)
+    {
+      cerr << "SimpleViewer: Couldn't start depth stream:" << openni::OpenNI::getExtendedError();
+      depth_stream.destroy();
+    }
+  }
+  else
+  {
+    cerr << "SimpleViewer: Couldn't find depth stream:" << openni::OpenNI::getExtendedError();
+  }
+
+  rc = color_stream.create(device, openni::SENSOR_COLOR);
+  if (rc == openni::STATUS_OK)
+  {
+    rc = color_stream.start();
+    if (rc != openni::STATUS_OK)
+    {
+      cerr << "SimpleViewer: Couldn't find color stream:" << openni::OpenNI::getExtendedError();
+      color_stream.destroy();
+    }
+  }
+  else
+  {
+    cerr << "SimpleViewer: Couldn't find color stream:" << openni::OpenNI::getExtendedError();
+  }
+
+
+  if (!depth_stream.isValid() || !color_stream.isValid())
+  {
+    cerr << "SimpleViewer: No valid streams. Exiting" << std::endl;
+    openni::OpenNI::shutdown();
+    return 2;
+  }
+
+
+  std::vector<openni::VideoStream*> streams;
+  streams.push_back(&color_stream);
+  streams.push_back(&depth_stream);
+
+  openni::VideoFrameRef color_frame;
+  openni::VideoFrameRef depth_frame;
+
+  device.setImageRegistrationMode(openni::IMAGE_REGISTRATION_DEPTH_TO_COLOR);
+
+  cv::Mat color_img, depth_img, depth_img_show;
+  cv::Mat color_to_depth, gray_img;
+
+  cv::ORB orb(200);
+  std::vector<KeyPoint> keypoints;
+
+  time_t start, end;
+  char fps[50];
+  int counter = 0;
+
+  while(true)
+  {
+	if (counter == 0){
+	    time(&start);
+	}
+
+    if(color_stream.readFrame(&color_frame) == openni::STATUS_OK)
+    if(color_frame.isValid())
+    {
+      color_img = getColorImage(color_frame);
+    }
+
+    if(depth_stream.readFrame(&depth_frame) == openni::STATUS_OK)
+    if(depth_frame.isValid())
+    {
+      depth_img = getDepthImage(depth_frame);
+      //depth_img_show = getDepthDrawableImage(depth_img);
+    }
+
+    color_to_depth = DS325Cali.mapColorToDepth(depth_img, color_img);
+    cvtColor(color_img, gray_img, CV_BGR2GRAY);
+
+    orb(gray_img, cv::Mat(), keypoints, cv::noArray());
+    drawKeypoints( color_img, keypoints, color_img, Scalar::all(-1), DrawMatchesFlags::DEFAULT );
+
+    // fps
+    time(&end);
+    counter++;
+    sprintf(fps, "Fps: %.2f", (double)counter/difftime(end, start));
+    if (counter == (INT_MAX - 1000))
+        counter = 0;
+
+    putText(color_img, fps , cvPoint(30,30), FONT_HERSHEY_COMPLEX_SMALL, 0.8, cvScalar(0,0,250), 1);
+
+    //cv::imshow("color to depth", color_to_depth);
+    //cv::imshow("depth", depth_img_show);
+    cv::imshow("color", color_img);
+
+    /*
+    if(!color_to_depth.empty() && !depth_img_show.empty())
+    {
+       cv::cvtColor(depth_img_show, depth_img_show, cv::COLOR_GRAY2BGR);
+       cv::Mat debug_img = color_to_depth * 0.5 + depth_img_show * 0.5;
+       cv::imshow("blend", debug_img);
+    }
+    */
+
+    int key = cv::waitKey(1);
+    if( key == 'q' || key == 27)
+    {
+      break;
+    }
+  }
+  return 0;
+}
